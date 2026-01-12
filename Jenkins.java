d9afbe991773f97011435ccb4942b59adb22f051
thanosisinevitable <ephorisrevival@proton.me>
2026-01-12 17:33:57 +0530

    Add files via upload

---
+++ Jenkins.java
@@ -0,0 +1,7 @@
+public class Jenkins {
+    public static void main(String[] args) {
+        System.out.println("Student Name: [Your Name]");
+        System.out.println("Register Number: [Your Reg Number]");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
